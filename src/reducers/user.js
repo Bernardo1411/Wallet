302abc51a14b79e082aed2696b7ae4b2d82c843e
bernardo1411 <bernardo1411@hotmail.com>
2021-09-29 17:25:39 -0300

    Project ready to be pushed to the remote repo

---
+++ src/reducers/user.js
@@ -0,0 +1,17 @@
+const initialState = {
+  email: '',
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'AUTH_SUCCESS':
+    return {
+      ...state,
+      ...{ email: action.email },
+    };
+  default:
+    return state;
+  }
+};
+
+export default authReducer;
